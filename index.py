f849980187e5df857cf5b8ddbd5c24dec05c3425
edwlad <80802093+edwlad@users.noreply.github.com>
2022-12-21 15:11:05 +0700

    fix

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from aiohttp import web
 
 async def main(req: web.Request, status: int = 200) -> web.Response:
     print('\nrun:', __name__, 'url:', req.url)
-    
+
     return web.Response(
         status=status,
         text=f'Home page.\n\nRun script: {__name__}, status code {status}'
