0630220719d4ad1890a536eb6b044314b5de23f1
edwlad <80802093+edwlad@users.noreply.github.com>
2022-12-25 20:31:20 +0700

    Добавлен поиск по году

--- stores_top.py
+++ stores_top.py
@@ -5,15 +5,17 @@ from datetime import date
 async def main(req: web.Request) -> web.Response:
     print('\nrun:', __name__, 'url:', req.url)
 
-    cnt = int(req.query.get('cnt', 10))
-    mm = int(req.query.get('mm', date.today().month))
+    query = req.query
+    cnt = int(query.get('cnt', 10))
+    mm = int(query.get('mm', date.today().month))
+    gg = int(query.get('gg', date.today().year))
 
     res = await req['pool'].fetch(f'''
         SELECT store.*, SUM(item.price) AS income
-        FROM sales, store, item
-        WHERE store.id = sales.store_id
-            AND item.id = sales.item_id
-            AND extract(month from sales.sale_time) = {mm}
+        FROM sales
+        JOIN store ON store.id = sales.store_id
+        JOIN item ON item.id = sales.item_id
+        WHERE to_char(sales.sale_time, 'YYYYMM') = '{gg}{mm:02}'
         GROUP BY store.id
         ORDER BY income DESC
         LIMIT {cnt}
